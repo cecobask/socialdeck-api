fcae8a11733dd35dce618c390cf7ed529a689a06
Tsvetoslav Dimov <baskski@gmail.com>
2019-10-21 22:03:36 +0100

    Added a basic frontend for the app, used in the labs.

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const {ApolloServer} = require('apollo-server-express');
 const schema = require('./schema');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const logger = require('morgan');
+const path = require('path');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const indexRouter = require('./routes/index');
 
 // Connect to the local MongoDB instance.
 mongoose.connect('mongodb://localhost:27017/usersDb', {
@@ -16,6 +21,32 @@ mongoose.connect('mongodb://localhost:27017/usersDb', {
 
 const app = express();
 app.use(express.json());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+
+// Catch 404 and forward to error handler.
+app.use(function (req, res, next) {
+    if (req.url === '/graphql') next();
+    else next(createError(404));
+});
+
+// Error handler.
+app.use(function (err, req, res, next) {
+    // Set locals, only providing error in development.
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // Render the error page.
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 app.listen({port: 4000}, () =>
     console.log(`🚀🚀🚀🚀Server ready at http://localhost:4000${server.graphqlPath}`)
 );
